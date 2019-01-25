7e5086b4197b2151c19e0a16c211e21c2ea09c08
Gadi Cohen <dragon@wastelands.net>
2019-01-25 14:15:25 +0200

    gongo-react: typecheck useGongoLive

--- gongo-react/index.js
+++ gongo-react/index.js
@@ -1,8 +1,13 @@
 import React, { useState, useEffect } from 'react';
 
 function useGongoLive(cursorFunc) {
+  if (typeof cursorFunc !== 'function')
+    throw new Error("useGongoLive expects a function that returns a cursor, "
+      + "not " + JSON.stringify(cursorFunc));
+
   const [cursorOrResults, setData] = useState(cursorFunc);
 
+  // TODO, check if user supplied a func that returns an array?  run twice?
   if (Array.isArray(cursorOrResults))
     return cursorOrResults;
 
@@ -10,6 +15,10 @@ function useGongoLive(cursorFunc) {
 
   const cursor = cursorOrResults;
   console.log('useGongoLive', cursor);
+
+  if (typeof cursor !== 'object' || !cursor.constructor || cursor.constructor.name !== 'Cursor')
+    throw new Error("useGongoLive function should return a cursor, not "
+      + "not " + JSON.stringify(cursor));
 
   const changeStream = cursor.watch();
   changeStream.on('change', change => {
